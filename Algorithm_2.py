025228b0d1773ace43b59c1362fdc000be1ee04f
5different <rlaalsgur464@gmail.com>
2019-01-04 15:13:15 +0900

    first commit

---
+++ Algorithm_2.py
@@ -0,0 +1,18 @@
+def ex2741():
+    A = int(input())
+    for i in range(1,A+1):
+        print(i)
+
+def ex2742():
+    A = int(input())
+    for j in range(A,0,-1):
+        print(j)
+
+def ex2739():
+    A = int(input())
+    for i in range(1,10):
+        print("{} * {} = {}".format(A,i,A*i))
+
+def ex2438():
+    A = int(input()) 
+    
