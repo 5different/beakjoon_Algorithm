16300206ff82bbf5d7dc04e74223eba2863ad8c7
5different <rlaalsgur464@gmail.com>
2019-01-04 18:04:32 +0900

    second commit

--- Algorithm_2.py
+++ Algorithm_2.py
@@ -13,6 +13,34 @@ def ex2739():
     for i in range(1,10):
         print("{} * {} = {}".format(A,i,A*i))
 
+
 def ex2438():
-    A = int(input()) 
     
+    A = int(input())
+    for i in range(1,A+1):
+        str_2 = []
+        for j in range(0,i):
+            str_2.append('*')        
+        print(','.join(str_2))
+ 
+def ex2439():
+    
+    A = int(input())
+    for i in range(1,A+1):
+        str_2 = []
+        for k in range(A-i,0,-1):
+            str_2.append(' ') 
+        for j in range(0,i):
+            str_2.append('*')        
+        print(''.join(str_2))
+
+def ex2440():
+    
+    A = int(input())
+    for i in range(1,A+1):
+        str_2 = []
+        for k in range(A+1-i,0,-1):
+            str_2.append('*')       
+        print(''.join(str_2))
+
+ex2440()
